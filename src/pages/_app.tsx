c10fd6897eac49231b27b9ccc1090856b9d73671
Luanaap <57630311+Luanaap@users.noreply.github.com>
2025-07-28 20:55:07 -0300

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
+ 
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src}       
+        width={150}
+        height={150} 
+        alt="Logo do Ignite"
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../styles";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
